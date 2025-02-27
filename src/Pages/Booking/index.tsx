855256e1f6513a80175fded154f77f96e814dad5
shuhrat7 <toshxujayivshuhrat7@gmail.com>
2025-02-27 19:31:46 +0500

    Add SignIn component with Google authentication; update package dependencies

--- src/Pages/Booking/index.tsx
+++ src/Pages/Booking/index.tsx
@@ -17,11 +17,15 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
+import { GoogleLogin } from "react-google-login";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
 import emailjs from "emailjs-com";
 import { BookingCard, BookingFrom, BookingWrapper } from "./style";
+import { ToastContainer, toast } from "react-toastify";
+import Footer from "../Home/Components/footer";
+import SignIn from "../../SignIn";
 interface Guide {
   id: string;
   guideName: string;
@@ -35,8 +39,6 @@ const supabaseKey =
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 function Booking() {
-  const [contact, setContact] = useState("");
-  const [days, setDays] = useState("");
   const { guideId } = useParams();
   const [guide, setGuide] = useState<any>(null);
   const [loading, setLoading] = useState(true);
@@ -46,12 +48,17 @@ function Booking() {
   const [isCodeSent, setIsCodeSent] = useState(false);
   const [activeStep, setActiveStep] = useState(0);
   const [formData, setFormData] = useState({
-    name: "",
+    peopleCount: "",
     email: "",
-    password: "",
+    transport: "",
   });
+
+  const [pickUpDate, setPickupDate] = useState("");
+  const [dropOffDate, setDropOffDate] = useState("");
+
   const [age, setAge] = React.useState("");
   const [transport, setTransport] = React.useState("female");
+
   useEffect(() => {
     const fetchGuide = async () => {
       if (!guideId) return;
@@ -71,42 +78,28 @@ function Booking() {
 
     fetchGuide();
   }, [guideId]);
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  if (!guide) {
-    return <p>No data found!</p>;
-  }
-
-  const handleBooking = () => {
-    if (!contact || !days) {
-      alert("Barcha maydonlarni to'ldiring!");
-      return;
-    }
-
-    // Guide'ga WhatsApp orqali xabar yuborish
+  const handleTransportChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setFormData({ ...formData, transport: event.target.value });
   };
+
   const sendVerificationCode = async () => {
     if (!email.trim()) {
       alert("Iltimos, email kiriting!");
       return;
     }
 
-    // ✅ Email formati to‘g‘ri ekanligini tekshiramiz
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email.trim())) {
       alert("Noto‘g‘ri email formati! To‘g‘ri email kiriting.");
       return;
     }
-    // 6 xonali tasdiqlash kodini yaratish
     const verificationCode = Math.floor(
       100000 + Math.random() * 900000
     ).toString();
     setGeneratedCode(verificationCode);
 
-    // EmailJS orqali xabar jo‘natish
     const templateParams = {
       to_email: email.trim(),
       verification_code: verificationCode,
@@ -122,18 +115,17 @@ function Booking() {
       );
 
       console.log("✅ Email yuborildi:", response);
-      alert("Tasdiqlash kodi emailga yuborildi!");
+      toast.success("Tasdiqlash kodi emailga yuborildi!");
     } catch (error: any) {
       console.error("❌ Xatolik:", error);
-      alert(`Kod yuborishda xatolik yuz berdi: ${error.text}`);
+      toast.error(`Kod yuborishda xatolik yuz berdi: ${error.text}`);
     }
   };
-  // 📌 2. Kiritilgan kodni tekshirish
   const verifyCode = () => {
     if (code === generatedCode) {
-      alert("✅ Email tasdiqlandi! Jarayon muvaffaqiyatli tugadi.");
+      toast.success("✅ Email tasdiqlandi! Jarayon muvaffaqiyatli tugadi.");
     } else {
-      alert("❌ Noto‘g‘ri kod. Qayta urinib ko‘ring.");
+      toast.error("❌ Noto‘g‘ri kod. Qayta urinib ko‘ring.");
     }
   };
   const steps = ["Foydalanuvchi Ma'lumotlari", "Qo'shimcha Ma'lumotlar"];
@@ -143,8 +135,8 @@ function Booking() {
   };
 
   const handleNext = () => {
-    if (activeStep === 0 && (!formData.name || !formData.email)) {
-      alert("Iltimos, barcha maydonlarni to'ldiring!");
+    if ((activeStep === 0 && !formData.peopleCount) || !formData.transport) {
+      toast.error("Iltimos, barcha maydonlarni to'ldiring!");
       return;
     }
 
@@ -160,152 +152,156 @@ function Booking() {
   const handleTransport = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTransport((event.target as HTMLInputElement).value);
   };
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!guide) {
+    return <p>No data found!</p>;
+  }
   return (
-    <Container maxWidth="xl">
-      <Header />
-      <BookingWrapper>
-        <BookingCard>
-          <img src={guide.guideImg} alt={guide.title} width="300" />
-          <Typography variant="body1">{guide.guideExperience}</Typography>
-        </BookingCard>
-        <BookingFrom>
-          <Box
-            sx={{
-              width: "100%",
-              margin: "auto",
-              padding: 4,
-              textAlign: "center",
-            }}
-          >
-            <Stepper activeStep={activeStep} alternativeLabel>
-              {steps.map((label, index) => (
-                <Step key={index}>
-                  <StepLabel>{label}</StepLabel>
-                </Step>
-              ))}
-            </Stepper>
+    <>
+      <Container maxWidth="xl">
+        <Header />
+        <BookingWrapper>
+          <BookingCard>
+            <img src={guide.guideImg} alt={guide.title} width="300" />
+            <Typography variant="body1">{guide.guideExperience}</Typography>
+          </BookingCard>
+          <BookingFrom>
+            <Box
+              sx={{
+                width: "100%",
+                margin: "auto",
+                padding: 4,
+                textAlign: "center",
+              }}
+            >
+              <Stepper activeStep={activeStep} alternativeLabel>
+                {steps.map((label, index) => (
+                  <Step key={index}>
+                    <StepLabel>{label}</StepLabel>
+                  </Step>
+                ))}
+              </Stepper>
 
-            {activeStep === 0 && (
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                }}
-              >
-                <TextField
-                  name="name"
-                  label="Ism"
-                  fullWidth
-                  margin="normal"
-                  value={formData.name}
-                  onChange={handleChange}
-                />
-                <FormControl sx={{ minWidth: 80, maxWidth: 150 }}>
-                  <InputLabel id="demo-simple-select-autowidth-label">
-                    Number of People
-                  </InputLabel>
-                  <Select
-                    labelId="demo-simple-select-autowidth-label"
-                    id="demo-simple-select-autowidth"
-                    value={age}
-                    onChange={handleCountOfPeople}
-                    autoWidth
-                    label="Number of People"
-                  >
-                    <MenuItem value="">
-                      <em>None</em>
-                    </MenuItem>
-                    <MenuItem value={20}>1</MenuItem>
-                    <MenuItem value={21}>2</MenuItem>
-                    <MenuItem value={22}>3</MenuItem>
-                    <MenuItem value={32}>4</MenuItem>
-                    <MenuItem value={42}>5</MenuItem>
-                    <MenuItem value={52}>6</MenuItem>
-                  </Select>
-                </FormControl>
-                <FormControl>
-                  <FormLabel id="demo-controlled-radio-buttons-group">
-                    Transport
-                  </FormLabel>
-                  <RadioGroup
-                    aria-labelledby="demo-controlled-radio-buttons-group"
-                    name="controlled-radio-buttons-group"
-                    value={transport}
-                    onChange={handleTransport}
-                  >
-                    <FormControlLabel
-                      value="Need"
-                      control={<Radio />}
-                      label="Need"
-                    />
-                    <FormControlLabel
-                      value="Needn't"
-                      control={<Radio />}
-                      label="Needn't"
-                    />
-                  </RadioGroup>
-                </FormControl>
-              </Box>
-            )}
+              {activeStep === 0 && (
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <FormControl sx={{ minWidth: 80, maxWidth: 150 }}>
+                    <InputLabel id="demo-simple-select-autowidth-label">
+                      Number of People
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-autowidth-label"
+                      id="demo-simple-select-autowidth"
+                      value={age}
+                      onChange={handleCountOfPeople}
+                      autoWidth
+                      label="Number of People"
+                    >
+                      <MenuItem value={20}>1</MenuItem>
+                      <MenuItem value={21}>2</MenuItem>
+                      <MenuItem value={22}>3</MenuItem>
+                      <MenuItem value={32}>4</MenuItem>
+                      <MenuItem value={42}>5</MenuItem>
+                      <MenuItem value={52}>6</MenuItem>
+                    </Select>
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel id="demo-controlled-radio-buttons-group">
+                      Transport
+                    </FormLabel>
+                    <RadioGroup
+                      aria-labelledby="demo-controlled-radio-buttons-group"
+                      name="controlled-radio-buttons-group"
+                      value={transport}
+                      onChange={handleTransportChange}
+                    >
+                      <FormControlLabel
+                        value="Need"
+                        control={<Radio />}
+                        label="Need"
+                      />
+                      <FormControlLabel
+                        value="Needn't"
+                        control={<Radio />}
+                        label="Needn't"
+                      />
+                    </RadioGroup>
+                  </FormControl>
+                  <SignIn />
+                </Box>
+              )}
 
-            {activeStep === 1 && (
-              <Box
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "1rem",
-                  width: "300px",
-                  margin: "auto",
-                }}
-              >
-                {!isCodeSent ? (
-                  // 📌 1-qadam: Emailni kiritish va kod yuborish
-                  <div>
-                    <h2>Email tasdiqlash</h2>
-                    <input
-                      type="email"
-                      placeholder="Emailingizni kiriting"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <button onClick={sendVerificationCode}>Kod yuborish</button>
-                  </div>
+              {activeStep === 1 && (
+                <Box
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "1rem",
+                    width: "300px",
+                    margin: "auto",
+                  }}
+                >
+                  {!isCodeSent ? (
+                    // 📌 1-qadam: Emailni kiritish va kod yuborish
+                    <div>
+                      <h2>Email tasdiqlash</h2>
+                      <input
+                        type="email"
+                        placeholder="Emailingizni kiriting"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                      <button onClick={sendVerificationCode}>
+                        Kod yuborish
+                      </button>
+                    </div>
+                  ) : (
+                    // 📌 2-qadam: Tasdiqlash kodini kiritish
+                    <div>
+                      <h2>Tasdiqlash kodi</h2>
+                      <input
+                        type="text"
+                        placeholder="Kod kiriting"
+                        value={code}
+                        onChange={(e) => setCode(e.target.value)}
+                      />
+                      <button onClick={verifyCode}>Tasdiqlash</button>
+                    </div>
+                  )}
+                </Box>
+              )}
+
+              <Box sx={{ marginTop: 2 }}>
+                {activeStep > 0 && (
+                  <Button onClick={handleBack} sx={{ marginRight: 2 }}>
+                    Orqaga
+                  </Button>
+                )}
+                {activeStep < steps.length - 1 ? (
+                  <Button variant="contained" onClick={handleNext}>
+                    Keyingisi
+                  </Button>
                 ) : (
-                  // 📌 2-qadam: Tasdiqlash kodini kiritish
-                  <div>
-                    <h2>Tasdiqlash kodi</h2>
-                    <input
-                      type="text"
-                      placeholder="Kod kiriting"
-                      value={code}
-                      onChange={(e) => setCode(e.target.value)}
-                    />
-                    <button onClick={verifyCode}>Tasdiqlash</button>
-                  </div>
+                  <Button variant="contained" color="success">
+                    Tugatish
+                  </Button>
                 )}
               </Box>
-            )}
-
-            <Box sx={{ marginTop: 2 }}>
-              {activeStep > 0 && (
-                <Button onClick={handleBack} sx={{ marginRight: 2 }}>
-                  Orqaga
-                </Button>
-              )}
-              {activeStep < steps.length - 1 ? (
-                <Button variant="contained" onClick={handleNext}>
-                  Keyingisi
-                </Button>
-              ) : (
-                <Button variant="contained" color="success">
-                  Tugatish
-                </Button>
-              )}
             </Box>
-          </Box>
-        </BookingFrom>
-      </BookingWrapper>
-    </Container>
+          </BookingFrom>
+        </BookingWrapper>
+      </Container>
+      <Footer />
+      <ToastContainer />
+    </>
   );
 }
 
